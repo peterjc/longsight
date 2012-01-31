181763683f312d4f639e391976ca8e5550c1c04f
peterjc <p.j.a.cock@googlemail.com>
2012-01-31 23:07:09 +0000

    Reading and setting the resolution

--- capture.py
+++ capture.py
@@ -9,18 +9,38 @@ Linux and Windows as well.
 """
 import sys
 try:
+    import cv #TODO - Where do the property constants live in cv2?
     import cv2
 except ImportError:
-    sys.stderr.write("Please install OpenCV and its cv2 Python interface\n")
+    sys.stderr.write("Please install OpenCV and the cv and cv2 Python interfaces\n")
     sys.exit(1)
 
+def get_resolution(video_capture):
+    return video_capture.get(cv.CV_CAP_PROP_FRAME_WIDTH), \
+           video_capture.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
+
+def set_resolution(video_capture, width, height):
+    video_capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, width)
+    video_capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, height)
+    assert (width, height) == get_resolution(video_capture), \
+        "Failed to set resoution to %i x %i" % (width, height)
+    return width, height
 
 vidcap = cv2.VideoCapture()
 assert vidcap.open(0)
 retval, image = vidcap.retrieve()
 assert retval, retval
 assert image is not None, image
-vidcap.release()
-assert cv2.imwrite("test.png", image)
+w, h = get_resolution(vidcap)
+assert w, h == image.size
+assert cv2.imwrite("test_%ix%i.png" % (w, h), image)
 
+print "Trying to change resolution..."
+w, h = set_resolution(vidcap, 640, 480)
+retval, image = vidcap.retrieve()
+assert retval, retval
+assert image is not None, image
+assert cv2.imwrite("test_%ix%i.png" % (w, h), image)
+
+vidcap.release()
 print "Done"
