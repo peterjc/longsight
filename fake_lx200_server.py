64007886c683f6314ae4e6789a9aefe89bdf058a
Peter Cock <p.j.a.cock@googlemail.com>
2014-01-12 19:49:09 +0000

    Record local site position & date/time.
    Currently using astropysics Site class for location and timezone.
    Will probably migrate this to using AstroPy in future, if and when
    that gains support for observation sites and conversion between
    local horizon coordinates (Alt/Az) and equatorial coordinates
    (RA/Dec).
    
    Storing desired site data/time as an offset (in seconds) from the
    system clock. This will be particularly useful on systems like a
    Raspberry Pi which lack a clock battery, and therefore a meaningful
    date if run offline.

--- fake_lx200_server.py
+++ fake_lx200_server.py
@@ -44,11 +44,24 @@ send commands St and Sg (for the location) then SG, SL, SC to set
 the time and date. If using "Meade LX-200 Classic" this imposes
 a 15s delay, using a newer model like the "Meade LX-200 GPS" there
 is no noticeable delay on connection.
-
 """
+
+#TODO: Alt/Az horizontal coordinates to equatorial, using local site's
+#location and time. See e.g.
+#http://pythonhosted.org/Astropysics/coremods/obstools.html#astropysics.obstools.Site
+#https://github.com/eteq/astropysics/issues/21
+#https://github.com/astropy/astropy-api/pull/6
+#http://infohost.nmt.edu/tcc/help/lang/python/examples/sidereal/ims/
+
 import socket
 import sys
 import commands
+import time
+import datetime
+
+#TODO - Try astropy if I can get it to compile on Mac OS X...
+from astropysics import coords
+from astropysics import obstools
 
 server_name = socket.gethostbyname(socket.gethostname())
 if server_name == "127.0.0.1":
@@ -57,6 +70,36 @@ if server_name == "127.0.0.1":
 server_port = 4030 #Default port used by SkySafari
 
 high_precision = False
+
+#Default to Greenwich, GMT - Latitude 51deg 28' 38'' N, Longitude zero
+local_site = obstools.Site(coords.AngularCoordinate("+51d28m38s"),
+                           coords.AngularCoordinate(0),
+                           tz=0)
+#Rather than messing with the system clock, will store any difference
+#between the local computer's date/time and any date/time set by the
+#client (which should match any location set by the client).
+local_time_offset = 0
+
+def site_time_gmt_as_epoch():
+    global local_time_offset
+    return time.time() + local_time_offset
+
+def site_time_gmt_as_datetime():
+    return datetime.datetime.fromtimestamp(site_time_gmt_as_epoch())
+
+def site_time_local_as_datetime():
+    global local_site
+    return site_time_gmt_as_datetime() - datetime.timedelta(hours=local_site.tz)
+
+def debug_time():
+    global local_site
+    if local_site.tz:
+        sys.stderr.write("Effective site date/time is %s (local time), %s (GMT/UTC)\n"
+                         % (site_time_local_as_datetime(), site_time_gmt_as_datetime()))
+    else:
+        sys.stderr.write("Effective site date/time is %s (local/GMT/UTC)\n"
+                         % site_time_gmt_as_datetime())
+
 
 def cm_sync():
     """For the :CM# command, Synchronizes the telescope's position with the currently selected database object's coordinates.
@@ -138,7 +181,14 @@ def set_site_latitude(value):
 
     Returns: 0 - Invalid, 1 - Valid
     """
-    return "1"
+    #Expect this to be followed by an Sg command to set the longitude...
+    global local_site
+    try:
+        local_site.latitude = coords.AngularCoordinate(value.replace("*", "d"))
+        return "1"
+    except Exception as err:
+        sys.stderr.write("Error with :St%s# latitude: %s\n" % (value, err))
+        return "0"
 
 def set_site_longitude(value):
     """For the :SgDDD*MM# command, Set current site longitude to DDD*MM
@@ -147,7 +197,15 @@ def set_site_longitude(value):
     """
     #Expected immediately after the set latitude command
     #e.g. :St+56*29# then :Sg003*08'#
-    return "1"
+    global local_site
+    try:
+        local_site.longitude = coords.AngularCoordinate(value.replace("*", "d"))
+        sys.stderr.write("Local site now latitude %0.3fd, longitude %0.3fdf\n"
+                         % (local_site.latitude.d, local_site.longitude.d))
+        return "1"
+    except Exception as err:
+        sys.stderr.write("Error with :Sg%s# longitude: %s\n" % (value, err))
+        return "0"
 
 def set_site_timezone(value):
     """For the :SGsHH.H# command, Set the number of hours added to local time to yield UTC
@@ -156,15 +214,44 @@ def set_site_timezone(value):
     """
     #Expected immediately after the set latitude and longitude commands
     #Seems the decimal is optional, e.g. :SG-00#
-    return "1"
+    global local_site
+    try:
+        local_site.tz = float(value) # Can in theory be partial hour, so not int
+        sys.stderr.write("Local site timezone now %s\n" % local_site.tz)
+        return "1"
+    except Exception as err:
+        sys.stderr.write("Error with :SG%s# time zone: %s\n" % (value, err))
+        return "0"
 
 def set_site_localtime(value):
     """For the :SLHH:MM:SS# command, Set the local Time
 
     Returns: 0 - Invalid, 1 - Valid
     """
+    global local_time_offset
+    local = time.time() + local_time_offset
     #e.g. :SL00:10:48#
-    return "1"
+    #Expect to be followed by an SC command to set the date.
+    try:
+        hh, mm, ss = (int(v) for v in value.split(":"))
+        if not (0 <= hh <= 24):
+            raise ValueError("Bad hour")
+        if not (0 <= mm <= 59):
+            raise ValueError("Bad minutes")
+        if not (0 <= ss <= 59):
+            raise ValueError("Bad seconds")
+        desired_seconds_since_midnight = 60*60*(hh + local_site.tz) + 60*mm + ss
+        t = time.gmtime(local)
+        current_seconds_since_midnight = 60*60*t.tm_hour + 60*t.tm_min + t.tm_sec
+        new_offset = desired_seconds_since_midnight - current_seconds_since_midnight
+        local_time_offset += new_offset
+        sys.stderr.write("Requested site time %i:%02i:%02i (TZ %s), new offset %is, total offset %is\n"
+                         % (hh, mm, ss, local_site.tz, new_offset, local_time_offset))
+        debug_time()
+        return "1"
+    except ValueError as err:
+        sys.stderr.write("Error with :SL%s# time setting: %s\n" % (value, err))
+        return "0"
 
 def set_site_calendar(value):
     """For the :SCMM/DD/YY# command, Change Handbox Date to MM/DD/YY
@@ -177,12 +264,33 @@ def set_site_calendar(value):
 
     Note: For LX200GPS/RCX400/Autostar II this is the UTC data!
     """
+    #Expected immediately after an SL command setting the time.
+    #
     #Exact list of values from http://www.dv-fansler.com/FTP%20Files/Astronomy/LX200%20Hand%20Controller%20Communications.pdf
     #return "1Updating        planetary data. #%s#" % (" "*32)
     #
-    #This seems to work but SkySafari takes a while to finish,
-    #making me guess it is expecting something else and times out?
-    return "1Updating Planetary Data#%s#" % (" "*30)
+    #This seems to work but SkySafari takes a while to finish
+    #if setup as a Meade LX200 Classic - much faster on other
+    #models.
+    #
+    #Idea is to calculate any difference between the computer's
+    #date (e.g. 1 Jan 1980 if the Raspberry Pi booted offline)
+    #and the client's date in days (using the datetime module),
+    #and add this to our offset (converting it into seconds).
+    #
+    global local_time_offset
+    #TODO - Test this in non-GMT/UTC other time zones, esp near midnight
+    current = datetime.date.fromtimestamp(time.time() + local_time_offset)
+    try:
+        wanted = datetime.date.fromtimestamp(time.mktime(time.strptime(value, "%m/%d/%y")))
+        days = (wanted - current).days
+        local_time_offset += days * 24 * 60 * 60 # 86400 seconds in a day
+        sys.stderr.write("Requested site date %s (MM/DD/YY) gives offset of %i days\n" % (value, days))
+        debug_time()
+        return "1Updating Planetary Data#%s#" % (" "*30)
+    except ValueError as err:
+        sys.stderr.write("Error with :SC%s# date setting: %s\n" % (value, err))
+        return "0"
 
 def return_one(value=None):
     """Dummy command implementation returning value 1."""
