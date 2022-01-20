d5b2948ed2d5fba3d4e246c9f3c561c28e7874fe
Craig Cmehil <ccmehil@gmail.com>
2022-01-20 23:38:11 +0100

    Update telescope_server.py

--- telescope_server.py
+++ telescope_server.py
@@ -205,10 +205,10 @@ def equatorial_to_alt_az(ra, dec, gst=None):
     az = atan2(-cos_dec*sin_h, cos_lat*sin_dec - sin_lat*cos_dec*cos_h)
     return alt, az % (2*pi)
 """
-    location = EarthLocation(lat=local_site.lat, lon=local_site.lon, height=0*u.m)
-    obs_time = Time()
-    alt_az_frame = AltAz(location=location, obstime=obs_time) 
-    return alt_az_frame
+location = EarthLocation(lat=local_site.lat, lon=local_site.lon, height=0*u.m)
+obs_time = Time()
+alt_az_frame = AltAz(location=location, obstime=obs_time) 
+return alt_az_frame
 #This test implicitly assumes time between two calculations not significant:
 _check_close((1.84096, 0.3984), alt_az_to_equatorial(*equatorial_to_alt_az(1.84096, 0.3984)))
 #_check_close(parse_hhmm("07:01:55"), 1.84096) # RA
