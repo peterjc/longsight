e040d32976ccf9a6a6fc72cad3a975ec1a4c8f5b
Craig Cmehil <ccmehil@gmail.com>
2022-01-25 22:57:56 +0100

    Update telescope_server.py

--- telescope_server.py
+++ telescope_server.py
@@ -434,6 +434,8 @@ def meade_lx200_cmd_St_set_latitude(value):
 
     Returns: 0 - Invalid, 1 - Valid
     """
+    if debug:
+        sys.stderr.write("meade_lx200_cmd_St_set_latitude: %s\n" % value)
     #Expect this to be followed by an Sg command to set the longitude...
     global local_site, config
     try:
