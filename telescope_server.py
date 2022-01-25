f6bc525710d2e8e56bffc020e5b1d2d6833a3b8d
Craig Cmehil <ccmehil@gmail.com>
2022-01-25 23:09:26 +0100

    Update telescope_server.py

--- telescope_server.py
+++ telescope_server.py
@@ -462,7 +462,7 @@ def meade_lx200_cmd_Sg_set_longitude(value):
         value = value.replace("*", "d")
         local_site.longitude = value
         sys.stderr.write("Local site now latitude %0.3fd, longitude %0.3fd\n"
-                         % (local_site.latitude.d, local_site.longitude.d))
+                         % (local_site.latitude, local_site.longitude))
         #That worked, should be safe to save the value to disk:
         config.set("site", "longitude", value)
         save_config()
