d825a5c9b2d605607606456c14f86136b2546d7a
Craig Cmehil <ccmehil@gmail.com>
2022-01-27 22:48:54 +0100

    Update telescope_server.py

--- telescope_server.py
+++ telescope_server.py
@@ -169,18 +169,21 @@ def alt_az_to_equatorial(alt, az, gst=None):
     sin_az = sin(az)
     cos_az = cos(az)
     # DEC based on latitude in radians
-    dec  = asin(sin_alt*sin_lat + cos_alt*cos_lat*cos_az)
-    debug_info("FUNCTION alt_az_to_equatorial - DEC from latitude: %s" % dec)
-    hours_in_rad = acos((sin_alt - sin_lat*sin(dec)) / (cos_lat*cos(dec)))
+    #dec  = asin(sin_alt*sin_lat + cos_alt*cos_lat*cos_az)
+    dec = Angle(az * u.deg)
+    dec.wrap_at('90d', inplace=True)
+
+    debug_info("FUNCTION alt_az_to_equatorial - DEC from latitude: %s" % dec.radian)
+    hours_in_rad = acos((sin_alt - sin_lat*sin(dec.radian)) / (cos_lat*cos(dec.radian)))
     if sin_az > 0.0:
         hours_in_rad = 2*pi - hours_in_rad
 
     # Now figure out RA based on Longitude in Radians
     ra = gst - site_longitude - hours_in_rad
-    debug_info("FUNCTION alt_az_to_equatorial - RA from longitude: %s" % dec)
+    debug_info("FUNCTION alt_az_to_equatorial - RA from longitude: %s" % dec.radian)
 
-    debug_info("FUNCTION alt_az_to_equatorial - actual values: ra %r - dec %r" % (ra % (pi*2), dec))
-    return ra % (pi*2), dec
+    debug_info("FUNCTION alt_az_to_equatorial - actual values: ra %r - dec %r" % (ra % (pi*2), dec.radian))
+    return ra % (pi*2), dec.radian
 
 def equatorial_to_alt_az(ra, dec, gst=None):
     debug_info("FUNCTION equatorial_to_alt_az - passed values: ra %r - dec %r" % (ra, dec))
