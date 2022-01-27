5d558037acf2e1b1603443039fdac39465ca3f60
Craig Cmehil <ccmehil@gmail.com>
2022-01-27 23:32:23 +0100

    Update telescope_server.py

--- telescope_server.py
+++ telescope_server.py
@@ -151,7 +151,7 @@ def greenwich_sidereal_time_in_radians():
     times = [now]
     t = Time(times, scale='utc')
     debug_info("FUNCTION greenwich_sidereal_time_in_radians - value %r" % t.sidereal_time('apparent', 'greenwich'))
-    return t.sidereal_time('apparent', 'greenwich')
+    return t.sidereal_time('apparent', 'greenwich').radian[0]
 
 def alt_az_to_equatorial(alt, az, gst=None):
     debug_info("FUNCTION alt_az_to_equatorial - passed values: alt %r - az %r" % (alt, az))
@@ -177,14 +177,13 @@ def alt_az_to_equatorial(alt, az, gst=None):
         hours_in_rad = 2*pi - hours_in_rad
 
     # Now figure out RA based on Longitude in Radians
-    debug_info("FUNCTION alt_az_to_equatorial - gst: %s" % gst[0])
+    debug_info("FUNCTION alt_az_to_equatorial - gst: %s" % gst)
     debug_info("FUNCTION alt_az_to_equatorial - hours_in_rad: %s" % hours_in_rad)
     debug_info("FUNCTION alt_az_to_equatorial - site_longitude: %s" % site_longitude)
     lon = Angle(site_longitude, u.radian)
     debug_info("FUNCTION alt_az_to_equatorial - lon: %s" % lon.radian)
     debug_info("FUNCTION alt_az_to_equatorial - Forumula: ra = gst - lon.radian - hours_in_rad")
-    #ra = gst[0] - lon.radian - hours_in_rad
-    ra = lon.radian - hours_in_rad
+    ra = gst - lon.radian - hours_in_rad
     debug_info("FUNCTION alt_az_to_equatorial - RA from longitude: %s" % dec)
     debug_info("FUNCTION alt_az_to_equatorial - actual values: ra %r - dec %r" % (ra % (pi*2), dec))
     return ra % (pi*2), dec
